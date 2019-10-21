3c8302d8d4ae71673805070454385abd57f8dc80
Felix Richter <judge@felixrichter.tech>
2019-10-21 14:20:59 +0200

    structure infos

--- hosts.py
+++ hosts.py
@@ -1,13 +1,21 @@
+# This class will hold all the information needed to configure any host 
 
 class Host:
 	def __init__(self, host_data = None, ip_data=None):
 		if not host_data is None:
-			_readin_host_data(host_data)
-		if not ip_data in  None:
-			_readin_ip_data(ip_data)
+			self._readin_host_data(host_data)
+		if not ip_data is  None:
+			self._readin_ip_data(ip_data)
 
 	def _readin_host_data(self, data):
-		pass
+		self.data = data
 
 	def _readin_ip_data(self, data):
-		pass
+		for ip in data:
+			if not ip["interface"] is None:
+				if not ip["interface"]["device"] is None:
+					if ip["interface"]["device"]["id"] == self.id:
+						print(ip)
+
+	def __getattr__(self, key):
+		return self.data[key]
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import inventory
 
-def main():
-	print(inventory.get_ip_data())
+from hosts import Host
 
+def main():
+	devices = inventory.get_device_data()
+	vms = inventory.get_vm_data()
+	ips = inventory.get_ip_data()
+
+	for d in devices:
+		host = Host(d, ips)
+		print(host.id, host.name, host.tags)
 
 if __name__ == "__main__":
 	main()
